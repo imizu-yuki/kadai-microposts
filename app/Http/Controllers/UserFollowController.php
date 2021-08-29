52cad42aa36ec873989c620b0289caf721429d01
imizu.yuki <student0107dd@gmail.com>
2021-08-29 05:22:43 +0000

    コントローラ

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -5,25 +5,25 @@ namespace App\Http\Controllers;
 class UserFollowController extends Controller
 {
     /**
-    * 投稿をお気に入り登録するアクション。
-    *
-    * @param $id 相手ユーザのid
-    * @return \Illuminate\Http\Response
-    */
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
     public function store($id)
     {
-        // 認証済みユーザ（閲覧者）が、 投稿をお気に入り登録する
-        \Auth::user()->favorite($microsoftId);
+        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
+        \Auth::user()->follow($id);
         // 前のURLへリダイレクトさせる
         return back();
     }
 
     /**
-    * 投稿のお気に入り登録を外すアクション。
-    *
-    * @param $id 相手ユーザのid
-    * @return \Illuminate\Http\Response
-    */
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
